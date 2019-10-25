3359d1566671452b893b072d52e2ace67374fec6
hyunwooPark <hyunwoo.park@ikoob.com>
2019-10-25 14:02:12 +0900

    merge

--- src/app/common/update.component.ts
+++ src/app/common/update.component.ts
@@ -1,50 +1,31 @@
 import { Component } from '@angular/core';
-import { UpdateService } from './services/update.service';
+import { UserService } from '../user/services/user.service';
 import { Router } from '@angular/router';
-import { FileUploader } from 'ng2-file-upload'
-
 @Component({
   selector: 'update',
   templateUrl: './update.component.html',
   styleUrls: ['./update.component.scss'],
-  providers: [UpdateService]
+  providers: [UserService]
 })
 export class UpdateComponent {
   /**
-   * 버전
+   * 사용자 식별자
    */
-  version: string;
+  userId: string;
   /**
-   * 배포일
+   * 사용자 비밀번호
    */
-  distDate: string;
-  /**
-   * 설명
-   */
-  discription: string;
-
-  uploader: FileUploader = new FileUploader({
-    url: 'http://localhost:5656/upload'
-  });
-
-  fileInfo = {
-    originalname: '',
-    filename: ''
-  };
+  userPw: string;
 
   /**
    * 업데이트 생성자다.
    * @param userSvc 사용자 서비스
    * @param router 라우터
    */
-  constructor(private updateService: UpdateService) {
-    this.updateService.getUpdateInfo().subscribe(res => {
-      this.version = res["version"];
-      this.distDate = res["distDate"];
-      this.discription = res["discription"];
-    });
+  constructor( 
+    private userSvc: UserService,
+    private router: Router) { }
 
-<<<<<<< HEAD
   /**
    * 로그인을 진행한다.
    */
@@ -61,16 +42,8 @@ export class UpdateComponent {
         }
         /**[TEMP - END] rest서비스 완료시 까지 임시 로직 */
 
-        confirm(error)
+
         console.log('error: ', error);
       });
-=======
-    this.uploader.onCompleteItem = (item, res, status, header) => {
-      this.version = res["version"];
-      this.distDate = res["distDate"];
-      this.discription = res["discription"];
-    }
->>>>>>> dc3bacd13957d8eadb722fc0be00e0e8aabe0888
   }
-
 }
